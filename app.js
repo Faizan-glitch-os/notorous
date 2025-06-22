a80d8138cc2453a51223077ca584b95919abfaad
Faizan Ahmad <faizanahmad2582001@gmail.com>
2025-06-22 17:43:05 +0500

    method added for updating existing tour using patch

--- app.js
+++ app.js
@@ -34,6 +34,18 @@ app.get('/api/v1/tours/:id', (req, res) => {
   res.status(200).json({ status: 'success', data: { tour } });
 });
 
+app.patch('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+
+  if (id > tours.length) {
+    return res
+      .status(404)
+      .json({ status: 'fail', message: 'Tour Not Found, Invalid id' });
+  }
+
+  res.status(200).json({ status: 'success', message: 'Tour updating...' });
+});
+
 app.post('/api/v1/tours', (req, res) => {
   const receivedData = req.body;
   const id = tours[tours.length - 1].id + 1;
