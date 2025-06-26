847595c7db8ca45658d31b56e5f2644ba468ce07
Faizan Ahmad <faizanahmad2582001@gmail.com>
2025-06-26 12:03:20 +0500

    virtuals and pre hooks added, name will no be slugified

--- controllers/tour-controller.js
+++ controllers/tour-controller.js
@@ -95,3 +95,69 @@ exports.updateTour = async (req, res) => {
     res.status(400).json({ status: 'fail', message: error });
   }
 };
+
+exports.getTourStats = async (req, res) => {
+  try {
+    const tourStats = await TourModel.aggregate([
+      { $match: { ratingsAverage: { $gte: 4.5 } } },
+      {
+        $group: {
+          _id: '$difficulty',
+          toursSum: { $sum: 1 },
+          numOfRatings: { $sum: '$ratingsQuantity' },
+          avgRatings: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' },
+        },
+      },
+      { $sort: { avgPrice: 1 } },
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      results: tourStats.length,
+      data: { tourStats },
+    });
+  } catch (error) {
+    res.status(404).json({ status: 'fail', message: error });
+  }
+};
+
+exports.getMonthlyPlan = async (req, res) => {
+  try {
+    const year = req.params.year * 1;
+
+    const monthlyPlan = await TourModel.aggregate([
+      {
+        $unwind: '$startDates',
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $month: '$startDates' },
+          numOfTours: { $sum: 1 },
+          tours: { $push: '$name' },
+        },
+      },
+      { $addFields: { month: '$_id' } },
+      { $project: { _id: 0 } },
+      { $sort: { numOfTours: 1 } },
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      results: monthlyPlan.length,
+      data: { monthlyPlan },
+    });
+  } catch (error) {
+    res.status(404).json({ status: 'fail', message: error });
+  }
+};
--- routes/tour-router.js
+++ routes/tour-router.js
@@ -5,6 +5,8 @@ const tourController = require('../controllers/tour-controller');
 const router = express.Router();
 
 router.route('/top-5-tours').get(tourController.getTop5Tours);
+router.route('/tour-stats').get(tourController.getTourStats);
+router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 
 router.route('/').get(tourController.getAllTours).post(tourController.addTour);
 
