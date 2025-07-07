379d5a459dfc3c0a850abf3dbaf97f6ba6ed353a
Faizan Ahmad <faizanahmad2582001@gmail.com>
2025-07-07 21:14:37 +0500

    development stopped here

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
+const helmet = require('helmet');
+const hpp = require('hpp');
 const AppError = require('./utils/app-error');
 const globalErrorHandler = require('./controllers/error-controller');
 
 const app = express();
+
+app.use(helmet());
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
@@ -15,6 +19,18 @@ const tourRouter = require('./routes/tour-router');
 const userRouter = require('./routes/user-router');
 
 app.use(express.json());
+app.use(
+  hpp({
+    whitelist: [
+      'price',
+      'difficulty',
+      'duration',
+      'ratingsAverage',
+      'ratingsQuantity',
+      'maxGroupSize',
+    ],
+  }),
+);
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
